59091f28aadbebb6a9021539da87a298b57d7981
nurmukhamedov <nurmuxamedovabdurasul@gmail.com>
2022-05-10 10:51:43 +0500

    lesson-13

--- project/src/components/useRef-basics/useRef.jsx
+++ project/src/components/useRef-basics/useRef.jsx
@@ -1,7 +1,42 @@
 import React, { useState, useEffect, useRef } from "react";
 
 const UseRef = () => {
-  return <div>useRef</div>;
+  const [inputValue, setInputValue] = useState("");
+  const count = useRef(0);
+  console.log(inputValue);
+
+  const refContainer = useRef(null);
+  const divContainer = useRef(null);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(refContainer.current.value);
+    console.log(divContainer.current);
+  };
+
+  useEffect(() => {
+    count.current = count.current + 1;
+
+    console.log(refContainer.current);
+  });
+  console.log(count);
+  return (
+    <>
+      <form className="form" onSubmit={handleSubmit}>
+        <div>
+          <input type="text" ref={refContainer} />
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    <div ref={divContainer}>Hello World</div>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={e => setInputValue(e.target.value)}
+      />
+      <h1>Render Count: {count.current}</h1>
+    </>
+  );
 };
 
 export default UseRef;
